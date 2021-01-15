7b9d18f25e0b3c69d4ed79cd9f44f32de6c211a4
th1rst <marco@kochannek.com>
2021-01-15 20:50:01 +0100

    added Bittrex API call and calculation

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ const App = () => {
     { name: "Poloniex", enabled: false },
   ]);
   const [binancePairs, setBinancePairs] = useState([]);
+  const [bittrexPairs, setBittrexPairs] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -68,7 +69,7 @@ const App = () => {
     getBittrexData();
   };
 
-  async function getBinanceData() {
+  const getBinanceData = async () => {
     try {
       //gets EVERY trading pair and its price from Binance
       let res = await axios.get("https://www.binance.com/api/v3/ticker/price");
@@ -76,34 +77,36 @@ const App = () => {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const formatBinanceData = (input) => {
     // since I only want BTC trading pairs, check if
     // last 3 characters of symbol match (i.e. "ETHBTC")
     const binanceBTCTradingPairs = [];
-    const relevantBinancePairs = [];
+    const relevantBinanceCoins = [];
 
     input.forEach((coin) => {
       if (coin.symbol.substring(coin.symbol.length - 3) === "BTC") {
-        binanceBTCTradingPairs.push({ name: coin.symbol, price: coin.price });
+        binanceBTCTradingPairs.push({
+          // remove the "BTC" again from the Trading pair
+          // to get the actual coin name (i.e. "ETHBTC" -> "ETH")
+          name: coin.symbol.substring(0, coin.symbol.length - 3),
+          price: coin.price,
+        });
       }
     });
 
-    //then cross-check with the topX (or default: top500) coins from CMC
+    // cross-check with the topX (or default: top500) coins from CMC
     binanceBTCTradingPairs.forEach((coin) => {
-      //remove the "BTC" again from the Trading pair name to get the coin name
-      const cointicker = coin.name.substring(0, coin.name.length - 3);
-
       //check if it's in the topX (default: top500)
       topCryptoTickers.forEach((topCrypto) => {
-        if (cointicker === topCrypto) {
+        if (coin.name === topCrypto) {
           //if it's in topX, push to array
-          relevantBinancePairs.push({ name: topCrypto, price: coin.price });
+          relevantBinanceCoins.push({ name: topCrypto, price: coin.price });
         }
       });
     });
-    setBinancePairs(relevantBinancePairs);
+    setBinancePairs(relevantBinanceCoins);
     setLoading(false);
   };
 
@@ -111,14 +114,53 @@ const App = () => {
     console.log("Getting Coinbase Data");
   };
 
-  const getBittrexData = () => {
-    console.log("Getting Bittrex Data");
+  const getBittrexData = async () => {
+    try {
+      //gets EVERY trading pair and its price from Bittrex
+      let res = await axios.get("https://api.bittrex.com/v3/markets/tickers");
+      formatBittrexData(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const formatBittrexData = (input) => {
+    // get BTC trading pairs
+    const bittrexBTCTradingPairs = [];
+    const relevantBittrexCoins = [];
+
+    input.forEach((coin) => {
+      if (coin.symbol.substring(coin.symbol.length - 3) === "BTC") {
+        bittrexBTCTradingPairs.push({
+          // Bittrex seperates trading pairs by dash, so remove it
+          // then remove "BTC" from it to get the actual coin name
+          name: coin.symbol
+            .split("-")
+            .join("")
+            .substring(0, coin.symbol.length - 4),
+          price: coin.lastTradeRate,
+        });
+      }
+    });
+
+    // cross-check with the topX (or default: top500) coins from CMC
+    bittrexBTCTradingPairs.forEach((coin) => {
+      //check if it's in the topX (default: top500)
+      topCryptoTickers.forEach((topCrypto) => {
+        if (coin.name === topCrypto) {
+          //if it's in topX, push to array
+          relevantBittrexCoins.push({ name: topCrypto, price: coin.price });
+        }
+      });
+    });
+    setBittrexPairs(relevantBittrexCoins);
+    setLoading(false);
   };
 
   return (
     <>
       <div className="flex flex-col items-center">
-        <h1 className="my-4 text-2xl font-semibold uppercas">Arbitunity</h1>
+        <h1 className="my-4 text-2xl font-semibold uppercase">Arbitunity</h1>
         <div className="w-full flex flex-row justify-center">
           {loading ? <h1>LOADING!</h1> : <h1>API HAS RESPONDED!</h1>}
         </div>
@@ -139,9 +181,13 @@ const App = () => {
 
             <div className="mx-4 border-2 border-blue-500">
               <div>
-                <h1>XXX relevant Bittrex Pairs:</h1>
+                <h1>{bittrexPairs.length} relevant Bittrex Pairs:</h1>
               </div>
-              {/* BITTREX PAIRS */}
+              <div className="flex flex-col items-center">
+                {bittrexPairs.map((crypto) => (
+                  <h3>{crypto.name}</h3>
+                ))}
+              </div>
             </div>
           </div>
         )}
