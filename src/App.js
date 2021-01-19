27705d5123b973ebc2d3ab49c6e189564efdd3d2
th1rst <marco@kochannek.com>
2021-01-19 02:37:49 +0100

    semi-finished all calculations, still WIP

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ const App = () => {
         if (!found) {
           uniqueCoinsAndPrices.push({
             name: coin.name,
-            prices: [
+            priceList: [
               {
                 exchange: `${exchange.name}`,
                 price: coin.price,
@@ -115,7 +115,7 @@ const App = () => {
             .map((element) => element.name)
             .indexOf(coin.name);
 
-          uniqueCoinsAndPrices[index].prices.push({
+          uniqueCoinsAndPrices[index].priceList.push({
             exchange: `${exchange.name}`,
             price: coin.price,
           });
@@ -123,15 +123,56 @@ const App = () => {
       });
     });
 
-    checkForLiquidity(uniqueCoinsAndPrices)
+    checkForLiquidity(uniqueCoinsAndPrices);
   };
 
-  const checkForLiquidity = () => {
+  const checkForLiquidity = (uniqueCoins) => {
+    let liquidCoins = [];
     // check if there are less than two prices available
     // (need at least two for doing arbitrage)
     // if not, remove it from the array
+    uniqueCoins.map((coin) => {
+      if (coin.priceList.length >= 2) {
+        liquidCoins.push(coin);
+      }
+    });
+    calculateArbitrageOpportunities(liquidCoins);
+  };
 
-  }
+  const calculateArbitrageOpportunities = (data) => {
+    // iterate over every coin
+    data.map((coin) => {
+      // then iterate over the respective price list,
+      // sort prices from low to high
+      let sortedArray = coin.priceList.sort((a, b) => a.price - b.price);
+
+      // first entry is the cheapest price, last entry the highest
+      const lowest = {
+        name: coin.name,
+        price: sortedArray[0].price,
+        exchange: sortedArray[0].exchange,
+      };
+      const highest = {
+        name: coin.name,
+        price: sortedArray[sortedArray.length - 1].price,
+        exchange: sortedArray[sortedArray.length - 1].exchange,
+      };
+      calculatePotentialPercentageGain(lowest, highest);
+    });
+  };
+
+  const calculatePotentialPercentageGain = (lowerNum, higherNum) => {
+    const percentageDiff =
+      ((higherNum.price - lowerNum.price) / lowerNum.price) * 100;
+    console.log(
+      `You can get an increase of ${percentageDiff.toFixed(1)} by buying ${
+        lowerNum.name
+      } from ${lowerNum.exchange} (price: {${lowerNum.price}}) and selling at ${
+        higherNum.exchange
+      } (price: ${higherNum.price})`
+    );
+    console.log(percentageDiff.toFixed(1));
+  };
 
   return (
     <>
